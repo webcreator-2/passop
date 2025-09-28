42bb91c943efa74d43b1f0c9ac3238c02424a551
Salwa Unnissa <salwashaik21@gmail.com>
2025-09-28 13:02:52 +0100

    changing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,23 +4,23 @@ const Navbar = () => {
   return (
     <nav className='bg-slate-800 text-white flex'>
       <div className='mycontainer flex justify-between px-3 py-4 h-14'>
-      <div className='logo font-bold text-2xl'>
-       
-        <span className='bg-green-500'> &lt;</span>
-        <span>Pass</span>
-        <span className='bg-green-500'>OP/&gt;</span>
+        <div className='logo font-bold text-2xl'>
+
+          <span className='bg-green-500'> &lt;</span>
+          <span>Pass</span>
+          <span className='bg-green-500'>OP/&gt;</span>
         </div>
-      {/* <ul>
-        <li className='flex gap-4'>
-          <a className='hover:font-bold' href="/">Home</a>
-          <a className='hover:font-bold' href="/">About</a>
-          <a className='hover:font-bold' href="/">Contact</a>
-        </li>
-        </ul> */}
-        <button className='text-white bg-green-700 w-[100px] h-[33px] rounded-full ring-white ring-1 flex justify-evenly'>
-          <img className='invert' src="icons/Githubb.png" width={50} height={40} alt="" />
-          <span className='font-semibold text-md p-1'>GitHub</span>
-        </button>
+
+        <a
+          href="https://github.com/webcreator-2"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white bg-green-700 w-[100px] h-[33px] rounded-full ring-white ring-1 flex justify-evenly items-center"
+        >
+          <img className="invert" src="icons/Githubb.png" width={20} height={20} alt="GitHub logo" />
+          <span className="font-semibold text-md p-1">GitHub</span>
+        </a>
+
       </div>
     </nav>
   )
